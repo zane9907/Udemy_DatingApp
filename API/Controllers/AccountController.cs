db0eaca6edc4e264f6c2bae028cb8e879d24c0e4
Botond Antalóczy <zane9907@gmail.com>
2022-09-17 19:23:47 +0200

    End of section 11 Photo uploading done

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x=>x.UserName.Equals(loginDto.Username.ToLower()));
+            var user = await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName.Equals(loginDto.Username.ToLower()));
 
             if (user == null) return Unauthorized("Invalid username");
 
@@ -41,7 +42,8 @@ namespace API.Controllers
             return new UserDto()
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
